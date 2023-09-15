ef569e5557063707a877f2328ebe31b9bf712e17
HeroesLament <nunya@business.org>
2023-09-15 14:39:30 -0800

    Fixed GNUradio testing defintion from boost to std

--- include/ale/decode_ff.h
+++ include/ale/decode_ff.h
@@ -36,7 +36,7 @@ namespace gr {
     class ALE_API decode_ff : virtual public gr::block
     {
      public:
-      typedef boost::shared_ptr<decode_ff> sptr;
+      typedef std::shared_ptr<decode_ff> sptr;
 
       /*!
        * \brief Return a shared_ptr to a new instance of ale::decode_ff.
