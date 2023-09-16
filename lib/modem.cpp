56380c7c2c7e4b24f7b30954693a6ef73d4bc572
HeroesLament <nunya@business.org>
2023-09-16 10:48:19 -0800

    Uploading modem.cpp WIP

--- lib/modem.cpp
+++ lib/modem.cpp
@@ -1,5 +1,17 @@
+#include <complex>
+
 namespace ale {
     namespace modem {
+
+        const int MAX_ALLOWED_BUFFER_SIZE = 5120;
+        const int SAMPLE_BUFFER_SIZE = 4096;
+        const int FFT_SIZE = 1024;
+        const float SAMPLING_RATE = 48000.0; // This is just an example value (48 kHz). Adjust to your actual sampling rate.
+        const float BASE_FREQUENCY = 1000.0; // Example value. Adjust to your actual base frequency.
+
+        const int BASE_FREQUENCY_BIN = static_cast<int>((BASE_FREQUENCY * FFT_SIZE) / SAMPLING_RATE);
+        const int FREQUENCY_SPACING_BIN = static_cast<int>((250 * FFT_SIZE) / SAMPLING_RATE);
+
 
         static const char* PREAMBLE_TYPES[] = {
             "[DATA]", "[THRU]", "[TO]", "[TWS]", "[FROM]", "[TIS]", "[CMD]", "[REP]"
@@ -10,6 +22,29 @@ namespace ale {
             DATA, THRU, TO, TWS, FROM, TIS, CMD, REP, NONE
         };
 
+        enum class MODEM_STATE {
+            IDLE,
+            SCANNING,
+            SOUNDING,
+            EXCHANGE,
+            LINKING,
+            LINKED,
+            RECEIVING_PREAMBLE,
+            RECEIVING_DATA,
+            SENDING_PREAMBLE,
+            SENDING_DATA
+        };
+
+        enum class MODEM_RETURNS {
+            SUCCESS = 0,
+            INVALID_LENGTH = -1,
+            NULL_POINTER = -2,
+            SAMPLE_GATHER_FAILURE = -3,
+            CARRIER_TONE_DETECTION_ERROR = -4,
+            SYNC_FAILURE = -5,
+            INVALID_SYMBOL = -6
+        };
+
         struct SYNC_RESULTS {
             MODEM_RETURNS resultCode;
             PREAMBLE_TYPE preambleType;
@@ -18,6 +53,7 @@ namespace ale {
         class Modem {
             private:
                 float sampleBuffer[SAMPLE_BUFFER_SIZE];
+                int ito2, itws2, ifrom2, itis2, irep2, idata2;
                 int currentSamplePosition = 0;  // Current position in sampleBuffer
                 int fft_history_offset;
                 std::complex<double> fft_out[FFT_SIZE];
@@ -25,26 +61,28 @@ namespace ale {
                 double fft_cs_twiddle[FFT_SIZE / 2];
                 double fft_ss_twiddle[FFT_SIZE / 2];
                 double fft_mag[FFT_SIZE / 2];
+                MODEM_STATE currentState;
+                MODEM_STATE previousState;
             public:
                 Modem();  // constructor
                 ~Modem(); // destructor
+                void resetHistory();
+                void computeTwiddles();
+                bool internalGather(float *buffer, int size);
                 MODEM_RETURNS processFFT(float *sample, int length);
                 MODEM_RETURNS gatherSample(float *sampleBuffer, int bufferSize);
                 MODEM_RETURNS detectCarrierTone(float *sample, int length);
+                MODEM_RETURNS modulateMFSK(int symbol, float* outputBuffer, int bufferSize);
+                MODEM_RETURNS demodulateMFSK(float *sampleBuffer, int bufferSize, int *outputSymbols, int &numSymbols);
                 SYNC_RESULTS synchronize(float* sample, int length);
+                PREAMBLE_TYPE decode(int symbol);
+                int encode(PREAMBLE_TYPE preamble);
         };
 
-        enum class MODEM_RETURNS {
-            SUCCESS = 0,
-            INVALID_LENGTH = -1,
-            NULL_POINTER = -2,
-            SAMPLE_GATHER_FAILURE = -3
-            CARRIER_TONE_DETECTION_ERROR = -4
-        };
         Modem::Modem() {
-            resetHistory();
+            Modem::resetHistory();
             fft_history_offset = 0;
-            computeTwiddles();
+            Modem::computeTwiddles();
         }
 
         void Modem::resetHistory() {
@@ -198,6 +236,87 @@ namespace ale {
             }
 
             return MODEM_RETURNS::SUCCESS;
+        }
+
+        MODEM_RETURNS Modem::demodulateMFSK(float *sampleBuffer, int bufferSize, int *outputSymbols, int &numSymbols) {
+            if (!sampleBuffer || !outputSymbols) {
+                return MODEM_RETURNS::NULL_POINTER;
+            }
+
+            int toneSamples = static_cast<int>(SAMPLING_RATE * 0.007); // 7ms worth of samples
+            if (bufferSize % toneSamples != 0) {
+                return MODEM_RETURNS::INVALID_LENGTH; // Ensure buffer size is a multiple of toneSamples for simplicity
+            }
+
+            numSymbols = bufferSize / toneSamples;
+
+            for (int i = 0; i < numSymbols; i++) {
+                float *currentTone = &sampleBuffer[i * toneSamples];
+
+                processFFT(currentTone, toneSamples);  // We assume this function updates the fft_mag array
+
+                int maxBin = 0;
+                for (int j = 1; j < FFT_SIZE / 2; j++) {  // We only need to inspect half the spectrum for real-valued signals
+                    if (fft_mag[j] > fft_mag[maxBin]) {
+                        maxBin = j;
+                    }
+                }
+
+                // Convert the maxBin to one of the 8 possible symbols
+                // This will depend on your base frequency, the spacing of the signals, and the FFT size and sampling rate
+                int symbol = (maxBin - BASE_FREQUENCY_BIN) / FREQUENCY_SPACING_BIN;
+
+                if (symbol < 0 || symbol >= 8) {
+                    return MODEM_RETURNS::CARRIER_TONE_DETECTION_ERROR;  // The detected tone is outside the expected range
+                }
+
+                outputSymbols[i] = symbol;
+            }
+
+            return MODEM_RETURNS::SUCCESS;
+        }
+
+
+        bool Modem::internalGather(float *buffer, int size) {
+            // Placeholder logic
+            return true; // This is just a placeholder. You need to implement the actual logic.
+        }
+
+        PREAMBLE_TYPE Modem::decode(int symbol) {
+            switch (symbol) {
+                case ito2:    return PREAMBLE_TYPE::TO;
+                case itws2:   return PREAMBLE_TYPE::TWS;
+                case ifrom2:  return PREAMBLE_TYPE::FROM;
+                default:      return PREAMBLE_TYPE::NONE;
+            }
+        }
+
+        int Modem::encode(PREAMBLE_TYPE preamble) {
+            switch (preamble) {
+                case PREAMBLE_TYPE::TO:    return ito2;
+                case PREAMBLE_TYPE::TWS:   return itws2;
+                case PREAMBLE_TYPE::FROM:  return ifrom2;
+                default:                   return -1; // Indicates error or unknown preamble
+            }
+        }
+
+        void transitionTo(MODEM_STATE newState) {
+            // Need to initialize previousState during object constructor
+            previousState = currentState;
+            currentState = newState;
+            // Any other logic you need during transition
+        }
+
+        void processData(float* data, int size) {
+            switch(currentState) {
+                case MODEM_STATE::IDLE:
+                    // ...
+                    break;
+                case MODEM_STATE::RECEIVING_PREAMBLE:
+                    // ...
+                    break;
+                // ... other states
+            }
         }
     }
 }
